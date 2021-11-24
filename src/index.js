2a54d112030a418d0316c260d8cdcea1b024dadc
Spoooods <neilandreitorre02@gmail.com>
2021-11-24 09:07:54 +0800

    removed pages folder, added shared folder

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import { render } from 'react-dom';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './components/login';
-import AdminHome from './components/admin/pages/homepage';
-import ProfHome from './components/admin/pages/homepage';
-import StudentHome from './components/student/pages/homepage';
+import AdminHome from './components/admin/homepage';
+import ProfHome from './components/admin/homepage';
+import StudentHome from './components/student/homepage';
 
 const rootElement = document.getElementById("root");
 render (
