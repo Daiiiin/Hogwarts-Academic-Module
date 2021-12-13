17c9f911aa9729102feb289c06dd6738a5beabdb
Sam <93627141+lourdsam@users.noreply.github.com>
2021-12-13 13:02:34 +0800

    Update professor.js

--- src/components/professor.js
+++ src/components/professor.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../stylesheet/homepage.css';
+import '../stylesheet/about.css';
 import event_pic from "../img/event_img.jpg";
 import { Outlet, Link } from "react-router-dom";
-import { Navbar, Container, Nav } from 'react-bootstrap';
-import mainLogo from "../img/school-logo.png"; 
+import { Navbar, Container, Nav, Table, Button } from 'react-bootstrap';
+import mainLogo from "../img/school-logo.png";
+import AboutUs from "../img/about_us.jpg";
+import Slytherin from "../img/slytherin.png";
+import Hufflepuff from "../img/hufflepuff.png";  
+import Gryffindor from "../img/gryffindor.png";    
+import Ravenclaw from "../img/ravenclaw.png";  
 import '../stylesheet/homepage.css';
 import { LogOut } from "./action";
 import Footer from "./footer";
@@ -71,4 +77,175 @@ export function ProfHeader() {
       </>
     );
   }
+
+  export function ManageCoursefromProf() {
+    return(
+        <>
+        <div className='ManageStudent'>
+          <Table striped bordered hover variant="dark">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Course Name</th>
+                <th>Student Name</th>
+                <th>Midterm Grade</th>
+                <th>Final Grade</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>Potions</td>
+                <td>Hermoine Granger</td>
+                <td>1.2</td>
+                <td>1.4</td>
+                <td>
+                <Button href="/professor/gradeadd" variant="info" type="submit">Add / Edit</Button>{' '}
+                </td>
+              </tr>
+            </tbody>
+          </Table>
+        </div>
+        <Footer />
+        </>
+    );
+}
+
+export function AddGrade() {
+  return(
+      <> 
+      <div className="manageStud">
+          <h1>Add Grade</h1>
+            <form>
+              <div class="form-group row">
+                <label class="col-sm-2 col-form-label">Midterm Grade:</label>
+                <div class="col-sm-10">
+                  <input type="text"class="form-control" placeholder="" ></input>
+                </div>
+              </div>
+              <div class="form-group row">
+                <label class="col-sm-2 col-form-label">Final Grade:</label>
+                <div class="col-sm-10">
+                  <input type="text"class="form-control" placeholder="" ></input>
+                </div>
+              </div>
+              <br></br>
+              <button class="btn btn-primary" type="submit">Submit</button>     
+            </form>
+            <br></br>
+            <button type="button"class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Edit</button>
+            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h5 class="modal-title" id="exampleModalLabel">Edit Course Grades</h5> 
+                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                      <form>
+                        <div class="form-group row">
+                          <label class="col-sm-2 col-form-label">Midterm Grade:</label>
+                          <div class="col-sm-10">
+                            <input type="text"class="form-control" placeholder="" ></input>
+                          </div>
+                        </div>
+                        <div class="form-group row">
+                          <label class="col-sm-2 col-form-label">Final Grade:</label>
+                          <div class="col-sm-10">
+                          <input type="text"class="form-control" placeholder="" ></input>
+                          </div>
+                        </div>
+                      </form>
+                    </div>
+                    <div class="modal-footer">
+                      <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                      <button type="button" class="btn btn-success">Save changes</button>
+                    </div>
+                  </div>
+                </div>
+              </div>  
+      </div>  
+      <Footer />
+      </>
+  );
+}
+
+export function AboutUsProf() {
+  return(
+      <>
+      <div className='about_page'>
+        <h1>Welcome to Hogwarts!</h1>
+        <div class="container">  
+          <div className='first'>
+            <img
+            src={AboutUs}
+            width="100%"
+            height="auto"
+            alt="About Us"
+            />
+          </div>  
+          <div className='second'>
+            <p>
+            Since its founding, Hogwarts has committed to offering the best educational experience in a structured, supportive environment. Our boarding and day program for boys in grades four through nine is unique in its ability to meet each student at his learning threshold and then truly maximize his potential. Hogrwarts parents frequently give us credit for providing what is a transformative experience for their sons.
+
+            Hogwarts was founded around 990 A.D.  Practicality and compassion drove the genesis, along with a commitment to providing a structured, supportive, and challenging academic environment. Hogwart's diverse offerings and character education have contributed to the development of superbly well-rounded young men for over a century. Hogwarts students, then and now, are encouraged to take risks with the knowledge that sometimes they will fail, but the support of their school community will never waver. Basic tenets such as determination, compassion, and tolerance guided Hillside students in 1901, just as they do today. The relevance of Hogwart's founding principles remains, and these values continue to shape the school community.
+            </p>
+          </div>   
+        </div>
+        <br></br>
+        <br></br>
+        <br></br>
+        <center><h1>THE FOUR HOUSES</h1></center>
+        <div class="container2">  
+          <div className='slytherin'>
+            <div><img
+            src={Slytherin}
+            width="100%"
+            height="auto"
+            alt="Slytherin"
+            /></div>
+            <div className='slytherin-text'><p>Our emblem is the serpent, the wisest of creatures; our house colours are emerald green and silver, and our common room lies behind a concealed entrance down in the dungeons. As you’ll see, its windows look out into the depths of the Hogwarts lake. We often see the giant squid swooshing by – and sometimes more interesting creatures. We like to feel that our hangout has the aura of a mysterious, underwater shipwreck.</p></div>
+          
+          </div>  
+          <div className='hufflepuff'>
+            <div><img
+            src={Hufflepuff}
+            width="100%"
+            height="auto"
+            alt="hufflepuff"
+            /></div>
+            <div className='hufflepuff-text'><p>Our emblem is the badger, an animal that is often underestimated, because it lives quietly until attacked, but which, when provoked, can fight off animals much larger than itself, including wolves. Our house colours are yellow and black, and our common room lies one floor below the ground, on the same corridor as the kitchens.</p></div>
+          
+          </div>   
+        </div>
+
+        <div class="container3">  
+          <div className='ravenclaw'>
+            <div>
+              <img
+                src={Ravenclaw}
+                width="100%"
+                height="auto"
+                alt="ravenclaw"
+                />
+            </div>
+            <div className='ravenclaw-text'><p>Our emblem is the eagle, which soars where others cannot climb; our house colours are blue and bronze, and our common room is found at the top of Ravenclaw Tower, behind a door with an enchanted knocker. The arched windows set into the walls of our circular common room look down at the school grounds: the lake, the Forbidden Forest, the Quidditch pitch and the Herbology gardens. No other house in the school has such stunning views.</p></div>
+          </div>  
+          <div className='gryffindor'>
+            <div><img
+            src={Gryffindor}
+            width="100%"
+            height="auto"
+            alt="gryffindor"
+            /></div>
+            <div className='gryffindor-text'><p>Our emblem is the lion, the bravest of all creatures; our house colours are scarlet and gold, and our common room lies up in Gryffindor Tower.</p></div>
+          </div>   
+        </div>
+                  
+      </div>
+      <Footer />
+      </>
+  );
+}
   
